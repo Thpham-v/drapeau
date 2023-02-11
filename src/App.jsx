086fdf2c4c97642dc3b7fa684778520d79c94ed9
Thibault P.V <Reus@reus186.home>
2023-02-11 10:09:50 +0100

    add homepage package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Body_P from './components/Body_P';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path='/' element={ <Body_P/> } >
 					<Route path='/' element={ <Home/> } />
 					<Route path='/about' element={ <About/> } />
 				</Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
